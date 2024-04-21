ff9a09ebe30de1da3f4121fdf1f62114af19848b
Foos Mahamud <foosmahamud55@gmail.com>
2024-04-20 20:27:44 -0500

    updated client.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,6 +1,16 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+ console.log(' in basket');
+
+function addItem () {
+ 
+
+
+}
+
+
 
 
 
